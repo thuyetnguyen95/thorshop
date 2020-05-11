8c8f30a8898baff8a4d3c0ad4f4489b5cd0e91bd
thuyetnv <thuyettiensinh@gmail.com>
2020-05-11 21:17:21 +0700

    Feature: upload product image

--- controllers/backend/product.js
+++ controllers/backend/product.js
@@ -4,10 +4,10 @@ const Category = require('../../models/category')
 const getProductStatus = (product) => {
   let status = {}
 
-  if (product.stock >= 5) {
+  if (product.stock > 5) {
     status.text = 'Đang còn'
     status.class = 'chip-success'
-  } else if (product.stock < 5 && product.stock > 0) {
+  } else if (product.stock <= 5 && product.stock > 0) {
     status.text = 'Sắp hết'
     status.class = 'chip-warning'
   } else {
@@ -43,10 +43,11 @@ module.exports = {
     let stock = parseInt(req.body.stock || 0)
     let price = parseInt(req.body.price || 0)
     let expiryAt = Date.parse(req.body.expiryAt) || new Date()
+    let image = req.body.fileUploadName
 
     if (!name) return res.redirect('thor/product/create')
 
-    let product = Product.save({ name, stock, price, expiryAt, categoryId })
+    let product = Product.save({ name, stock, price, expiryAt, categoryId, image })
 
     return product ? res.redirect('/thor/product') : res.redirect('/thor/product/create')
   },
