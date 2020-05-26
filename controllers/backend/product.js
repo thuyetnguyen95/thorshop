2f233ce5ad8f7a323e0de245ebf3a5faa2a09807
thuyetnv <thuyettiensinh@gmail.com>
2020-05-27 01:15:40 +0700

    HOTFIX: disable remove product with unexpected error

--- controllers/backend/product.js
+++ controllers/backend/product.js
@@ -147,12 +147,14 @@ module.exports = {
   },
 
   deleteProduct: (req, res) => {
-    let id = req.params.id || ''
-
-    //TODO: implement fail case
-    Product.remove(id)
-
     return res.redirect('/thor/product')
+    
+    // let id = req.params.id || ''
+
+    // //TODO: implement fail case
+    // Product.remove(id)
+
+    // return res.redirect('/thor/product')
   },
 
   importProduct: (req, res) => {
